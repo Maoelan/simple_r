9222310e629fbf671f35b0fbb1ba7c3e6c8cdcbe
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2024-05-31 18:09:53 +0700

    Create example_viz_3.r

---
+++ example_viz_3.r
@@ -0,0 +1,19 @@
+ggplot(data=penguins)+
+  geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g))+
+  geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+
+ggplot(data=penguins)+
+  geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g,linetype=species))
+
+ggplot(data=penguins)+
+  geom_jitter(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut,color=cut))
+
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut,fill=cut))
+
+ggplot(data=diamonds)+
+  geom_bar(mapping=aes(x=cut,fill=clarity))
+
